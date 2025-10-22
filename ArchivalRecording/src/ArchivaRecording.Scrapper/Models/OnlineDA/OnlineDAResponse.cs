bd503f79818cb2a506a732edd7b41b5c00e9c2eb
Tommy Truong <glebme@yahoo.com>
2025-10-23 01:13:14 +1100

    Change field setting to init

--- ArchivalRecording/src/ArchivaRecording.Scrapper/Models/OnlineDA/OnlineDAResponse.cs
+++ ArchivalRecording/src/ArchivaRecording.Scrapper/Models/OnlineDA/OnlineDAResponse.cs
@@ -2,43 +2,43 @@ namespace DevelopmentProposalScrapper.Models.OnlineDA;
 
 public record OnlineDAResponse
 {
-    public int PageSize { get; }
-    public int PageNumber { get; }
-    public int TotalPages { get; }
-    public int TotalCount { get; }
-    public IEnumerable<DevelopmentApplication> DevelopmentApplications { get; }
+    public int PageSize { get; init; }
+    public int PageNumber { get; init; }
+    public int TotalPages { get; init; }
+    public int TotalCount { get; init; }
+    public IEnumerable<DevelopmentApplication> DevelopmentApplications { get; init; }
 }
 
 // TODO Check if any fields can be null
 public record DevelopmentApplication
 {
-    public string PlanningPortalApplicationNumber { get; }
-    public ApplicationType ApplicationType { get; }
-    public ApplicationStatus ApplicationStatus { get; }
-    public string CouncilName { get; }
-    public IEnumerable<ProposedDevelopmentType> DevelopmentType { get; }
-    public DateOnly LodgementDate { get; }
-    public DateOnly DeterminationDate { get; }
-    public int CostOfDevelopment { get; }
-    public IEnumerable<Address> Location { get; }
+    public string PlanningPortalApplicationNumber { get; init; }
+    public ApplicationType ApplicationType { get; init; }
+    public ApplicationStatus ApplicationStatus { get; init; }
+    public string CouncilName { get; init; }
+    public IEnumerable<ProposedDevelopmentType> DevelopmentType { get; init; }
+    public DateOnly LodgementDate { get; init; }
+    public DateOnly DeterminationDate { get; init; }
+    public int CostOfDevelopment { get; init; }
+    public IEnumerable<Address> Location { get; init; }
 }
 
 public record Address
 {
-    public string FullAddress { get; }
-    public string X { get; }
-    public string Y { get; }
-    public string StreetNumber1 { get; }
-    public string StreetNumber2 { get; }
-    public string StreetNumber3 { get; }
-    public string StreetName { get; }
-    public string StreetType { get; }
-    public string Suburb { get; }
-    public string Postcode { get; }
-    public string State { get; }
+    public string FullAddress { get; init; }
+    public string X { get; init; }
+    public string Y { get; init; }
+    public string StreetNumber1 { get; init; }
+    public string StreetNumber2 { get; init; }
+    public string StreetNumber3 { get; init; }
+    public string StreetName { get; init; }
+    public string StreetType { get; init; }
+    public string Suburb { get; init; }
+    public string Postcode { get; init; }
+    public string State { get; init; }
 }
 
 public record ProposedDevelopmentType
 {
-    public string DevelopmentType { get; }
+    public string DevelopmentType { get; init; }
 }
