cb9f7aac1b3c02c1410a30f525661e4f4e20b67a
Tommy Truong <glebme@yahoo.com>
2026-03-20 23:38:43 +1100

    Use string instead of class filters

--- ArchivalRecording/src/ArchivalRecording.Scrapper/Services/OnlineDA/IOnlineDAApi.cs
+++ ArchivalRecording/src/ArchivalRecording.Scrapper/Services/OnlineDA/IOnlineDAApi.cs
@@ -6,5 +6,5 @@ namespace DevelopmentProposalScrapper.Services.OnlineDA;
 public interface IOnlineDAApi
 {
     [Get("/eplanning/data/v0/OnlineDA")]
-    Task<IApiResponse<OnlineDAResponse>> GetOnlineDARecordsAsync([Header("PageSize")] int pageSize, [Header("PageNumber")] int pageNumber, [Header("Filters")] Filters filters);
+    Task<IApiResponse<OnlineDAResponse>> GetOnlineDARecordsAsync([Header("PageSize")] int pageSize, [Header("PageNumber")] int pageNumber, [Header("Filters")] string filters);
 }
