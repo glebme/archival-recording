4b520b53046375840c6677b8b35cd96956279b7e
Tommy Truong <glebme@yahoo.com>
2026-05-12 10:13:37 +1000

    Add Polly for resilience and implement retry logic in service extensions

--- ArchivalRecording/src/ArchivalRecording.Scrapper/Infrastructure/InfrastructureServiceExtensions.cs
+++ ArchivalRecording/src/ArchivalRecording.Scrapper/Infrastructure/InfrastructureServiceExtensions.cs
@@ -1,6 +1,8 @@
 using DevelopmentProposalScrapper.Domain.Repositories;
 using DevelopmentProposalScrapper.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Polly;
+using Polly.Retry;
 
 namespace DevelopmentProposalScrapper.Infrastructure;
 
@@ -14,7 +16,21 @@ public static class InfrastructureServiceExtensions
                 npgsl => npgsl.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)
             )
         );
-        
+
         services.AddScoped<IDevelopmentApplicationRepository, DevelopmentApplicationRepository>();
+
+        var retryPipeline = new ResiliencePipelineBuilder()
+            .AddRetry(new RetryStrategyOptions
+            {
+                MaxRetryAttempts = 3,
+                Delay = TimeSpan.FromSeconds(2),
+                BackoffType = DelayBackoffType.Exponential,
+                ShouldHandle = new PredicateBuilder()
+                    .Handle<TimeoutException>()
+                    .Handle<InvalidOperationException>()
+            })
+            .Build();
+
+        services.AddSingleton(retryPipeline);
     }
 }
